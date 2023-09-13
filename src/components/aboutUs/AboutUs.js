65f6aad0a37a35b19aa513ef2961fec4defaf2f0
Вадим <andreeshinvadim13@gmail.com>
2023-09-13 16:34:32 +0300

    Almost finished top header

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -3,14 +3,16 @@ import "../aboutUs/aboutUs.css"
 const AboutUs = () => {
   return (
     <div className="App-body">
-      <h2>About Us</h2>
-      <p>
+      <h2 className="app_body">About Us</h2>
+      <p className="app_body">
         Extremity sweetness difficult behaviour he of. On disposal of as
-        landlord horrible. Afraid at highly months do things on at. Situation
-        recommend objection do intention so questions. As greatly removed
-        calling pleased improve an. Last ask him cold feel met spot shy want.
-        Children me laughing we prospect answered followed. At it went is song
-        that held help face. <br />
+        landlord horrible.
+        <br /> Afraid at highly months do things on at. Situation recommend
+        objection do intention so questions. As greatly removed calling pleased
+        improve an. Last ask him cold feel met spot shy want. Children me
+        laughing we prospect answered followed. At it went is song that held
+        help face. <br />
+        <br />
         Now residence dashwoods she excellent you. Shade being under his bed
         her, Much read on as draw. Blessing for ignorant exercise any yourself
         unpacked. Pleasant horrible but confined day end marriage. Eagerness
