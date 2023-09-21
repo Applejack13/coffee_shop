4ad87087041305f6b9973cce54d9850b923b99b1
Вадим <andreeshinvadim13@gmail.com>
2023-09-21 18:49:41 +0300

    Finished Third Page
    Have to fix all problems

--- src/components/imgAndTextContainer/ImgAndTextContainer.js
+++ src/components/imgAndTextContainer/ImgAndTextContainer.js
@@ -1,5 +1,15 @@
 import "../imgAndTextContainer/imgAndTextContainer.css"
 
-const ImgAndTextContainer = () => {}
+const ImgAndTextContainer = ({ img, h, p }) => {
+  ;<div className="containerImgAndText">
+    <div className="first_block">
+      <img src={img} alt="girl_and_coffee" />
+    </div>
+    <div className="second_block">
+      <h2>{h}</h2>
+      <p>{p}</p>
+    </div>
+  </div>
+}
 
 export default ImgAndTextContainer
--- src/components/thirdPageMain/ThirdPageMain.js
+++ src/components/thirdPageMain/ThirdPageMain.js
@@ -1,9 +1,30 @@
 import "../thirdPageMain/thirdPageMain.css"
+import LinesAndBeans from "../linesAndBeans/LinesAndBeans"
+import coffeeBeans4 from "../../images/coffee-beans 4.svg"
+import aromitico_coffee from "../../images/aromico_coffee.jpg"
 
 const ThirdPageMain = () => {
   return (
     <div className="thirdPageMain">
-      <h1>Third Page Main</h1>
+      <div className="first_block_trird_page">
+        <img src={aromitico_coffee} alt="aromitico_coffee" />
+      </div>
+      <div className="second_block_trird_page">
+        <h2>About it</h2>
+        <LinesAndBeans img={coffeeBeans4} bgc="black" />
+        <p>
+          <b>Country:</b> Brazil
+        </p>
+        <p>
+          <b>Description:</b> Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          <b>Price:</b> <span>16.99$</span>
+        </p>
+      </div>
     </div>
   )
 }
